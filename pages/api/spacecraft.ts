60f82f550965b84be91ccc57afa14e54b8ed611d
jingjielim <jingjie3000@gmail.com>
2024-12-03 07:03:35 +0800

    feat: Add cookie support
    Add tough-cookie and axios-cookiejar-support to save cookies after login
    Axios does not support cookies out of the box
    A wrapper axios-cookiejar-support is used to save cookies for
    subsequent use

--- pages/api/spacecraft.ts
+++ pages/api/spacecraft.ts
@@ -1,39 +1,50 @@
 import axios from "axios";
+import { wrapper } from "axios-cookiejar-support";
+import { CookieJar } from "tough-cookie";
 
 const base_url = process.env.SPACETRACK_URL || "";
 const login_request = process.env.SPACETRACK_LOGIN_REQUEST || "";
 const tle_query = process.env.SPACETRACK_TLE_QUERY || "";
 const username = process.env.SPACETRACK_USERNAME || "";
 const password = process.env.SPACETRACK_PASSWORD || "";
-axios.defaults.withCredentials = true;
+
+const jar = new CookieJar();
+
+const axiosInstance = wrapper(
+  axios.create({
+    baseURL: base_url,
+    jar,
+    withCredentials: true,
+  })
+);
 
 async function getSpacecraftData() {
-  return axios
-    .post(
-      `${base_url}${login_request}`,
-      {
-        identity: username,
-        password: password,
-      },
-      { withCredentials: true }
-    )
+  return axiosInstance
+    .post(`${base_url}${login_request}`, {
+      identity: username,
+      password: password,
+    })
     .then((response) => {
       console.log("Login response", response.headers);
-      console.log("Login data", response.data)
+      console.log("Login data", response.data);
       if (response.status !== 200) {
         throw Error("Login failed");
       }
 
-     return axios.get(`${base_url}${tle_query}`, {
-        withCredentials: true,
-      });
-    }).then((dataResponse) => {
-        console.log("Received SpacecraftData: ", dataResponse.headers);
-        return dataResponse.data;
+      return axiosInstance.get(`${base_url}${tle_query}`);
+    })
+    .then((dataResponse) => {
+      console.log("Received SpacecraftData: ", dataResponse.data);
+      return dataResponse.data;
+    })
+    .catch((error) => {
+      console.error("Error fetching spacecraft data:", error);
+      throw error;
     });
 }
 
 export default async function handler(req: any, res: any) {
   const spacecraftData = await getSpacecraftData();
+  console.log("Spacecraft Data fetched successfully", spacecraftData);
   res.status(200).json({ data: spacecraftData });
 }
